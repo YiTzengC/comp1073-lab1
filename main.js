d8c76b166a499e000461b753109805da9b48d329
YiTzengC <200437546@student.georgianc.on.ca>
2021-01-26 21:30:46 -0500

    finish lab one

--- main.js
+++ main.js
@@ -37,22 +37,49 @@ Vinay
 Sahil
 Kaushal`;
 
-let students = names.split('\n');
+let students = names.split('\n'); //Requirement 3
 
-const instructor = 'Jessica Gilfillan';
+const instructor = 'Jessica Gilfillan'; //Requirement 4
 
-let welcomeType;
+let welcomeType; //Requirement 5
 
-function welcomeCOMP1073(){
+function welcomeCOMP1073(){ //Requirement 6
+
+    var greetings = ['Welcome ', 'Hi ', 'Good to see you again, '];
     if(welcomeType === 'learner'){
         students.forEach((studentName)=>{
-            console.log(studentName);
+            appendElement(greetings[Math.floor(Math.random()*3)] + studentName + '.', 'student');
         })
     }else if(welcomeType === 'instructor'){
-        console.log(instructor);
+        appendElement(greetings[Math.floor(Math.random()*3)] + instructor + '.', 'instructor');
     }
+    //auto srcoll to bottom
+    var scrollOptions = {
+        left: 0,
+        top: document.querySelector('#welcome').scrollHeight,
+        behavior: 'smooth'
+    }
+    document.querySelector('#welcome').scrollTo(scrollOptions);
+
 }
 
-welcomeType = 'learner';
+function appendElement(text, styleClass){
+    var tag = document.createElement('span');
+    tag.className = 'badge rounded-pill ' + styleClass;
+    tag.innerHTML = text;
+    var element = document.getElementById('welcome');
+    element.appendChild(tag);
+    console.log(text);
+}
 
-welcomeCOMP1073();
+welcomeType = 'learner'; //Requirement 7
+
+welcomeCOMP1073(); //Requirement 8
+
+//for bonus 
+//select different welcomeType to demo
+const selectLinstener = document.querySelector('select');
+selectLinstener.onchange = function(event){
+    welcomeType = selectLinstener.value;
+    welcomeCOMP1073();
+}
